531f25f5cef37b2db94d19d0024b3164e68a4cc5
Adarsh <adarsh@thevibegod.com>
2020-01-02 17:18:48 +0530

    Updates

---
+++ precache-manifest.7bf56152d7bdec0fb5577a715b36a953.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff73eaa6792c1cd85c512a7c66d70ac",
+    "url": "/EMS/index.html"
+  },
+  {
+    "revision": "85486cbd81b20302947a",
+    "url": "/EMS/static/css/2.55da68c4.chunk.css"
+  },
+  {
+    "revision": "85486cbd81b20302947a",
+    "url": "/EMS/static/js/2.5cb44933.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/EMS/static/js/2.5cb44933.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6cc4c0fcf5c9b230c5f",
+    "url": "/EMS/static/js/main.658a5599.chunk.js"
+  },
+  {
+    "revision": "35839405fe4fe4f53088",
+    "url": "/EMS/static/js/runtime-main.a0bf52a5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/EMS/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/EMS/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/EMS/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/EMS/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/EMS/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
